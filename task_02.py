8941b717a6dbd48b18a869d1f6094a8620b9ba86
jchan3 <joechan2014@gmail.com>
2016-09-18 01:51:20 +0000

    commit

--- task_02.py
+++ task_02.py
@@ -4,3 +4,13 @@
 
 
 import inquisition
+
+STRING1 = 'Spanish'
+STRING2 = 'Flemish'
+LENGTH = len(STRING1)
+SPANISH_INDEX = (inquisition.SPANISH).index(STRING1)
+STRING3 = (inquisition.SPANISH)[0:SPANISH_INDEX]
+NEW_INDEX = SPANISH_INDEX + LENGTH
+STRING4 = (inquisition.SPANISH)[NEW_INDEX:]
+
+FLEMISH = STRING3 + STRING2 + STRING4
